b101fb8a93e4bdb36e9cb298544361ed8b2cbbfe
Student <student@RU-106.local>
2016-01-25 16:03:33 -0700

    more functions

---
+++ main.js
@@ -0,0 +1,79 @@
+// 1 - Need Help
+
+// var getName = function() {
+// 	var obj = {}
+// 	console.log(obj.name);
+// }
+
+// getName({ name: "Luisa", age: 25 });
+
+// 2 - Done
+
+// var totalLetters = function(myArray) {
+// 	var total = 0
+// 	for (var i=0; i < myArray.length; i++){
+// 		total += (myArray[i].length);
+	
+// 	}
+// 	return(total);
+// }
+
+
+// totalLetters(['what', 'happened', 'to', 'my', 'function']);
+
+// 3 - Need Help
+
+var keyValue = function(a, b){
+
+	for(var a in keyValue) {
+		return {c};
+		console.log(c);
+	}
+	
+}
+keyValue('city', 'Denver');
+
+
+// var keyValue = function(z, b){
+
+// 	var obj2 = {};
+// 	var city = keyValue[z];
+// 	var loc = keyValue[b];
+// 	obj2 = {city: loc};
+// 	console.log(obj2);
+// }
+// keyValue('city', 'Denver');
+
+// 4 - Done
+
+// var negativeIndex = function(arrayp, x){
+// 	for (var i=0; i < arrayp.length; i++) {
+// 		b = arrayp.length + x;
+// 		return(arrayp[b]);
+// 	}
+// }
+
+// negativeIndex(['a', 'b', 'c', 'd', 'e'], -3)
+
+
+// 5 - Done
+
+// var removeM = function(str) {
+// 	x = str.replace(/m/g, '');
+// 	console.log(x);
+// }
+// removeM('memory');
+
+//  6 - Done
+
+// var printObject = function(b){
+
+// 	for (var key in b) {
+// 		console.log(key + " is " + b[key]);
+// 	}
+	
+// }
+// printObject({ a: 10, b: 20, c: 30 });
+
+
+
